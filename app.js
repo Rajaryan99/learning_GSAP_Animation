8e614b34e5e3f995e3c3c3b6ab530f1b1f7f28db
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-11 15:37:38 +0530

    Gsap.to and Gsap.form

---
+++ app.js
@@ -0,0 +1,21 @@
+gsap.to('.box', {
+    x: 500,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "pink",
+    borderRadius: "50%",
+    scale: 0.5
+})
+
+gsap.from('.box2', {
+    x: 500,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "pink",
+    borderRadius: "50%",
+    scale: 0.5
+})
+
+
