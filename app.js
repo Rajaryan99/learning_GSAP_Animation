be50fc1a1d71904442183d9f806e8a2fcf8b01b3
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-11 15:53:27 +0530

    yoyo and repeat property in gsap

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-// gsap.to('.box', {
-//     x: 500,
-//     duration: 2,
-//     delay: 1,
-//     rotate: 360,
-//     backgroundColor: "pink",
-//     borderRadius: "50%",
-//     scale: 0.5
-// })
+gsap.to('.box', {
+    x: 500,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "pink",
+    borderRadius: "50%",
+    scale: 0.5,
+    yoyo: true,
+    repeat: -1
+})
 
-// gsap.from('.box2', {
-//     x: 500,
-//     duration: 2,
-//     delay: 1,
-//     rotate: 360,
-//     backgroundColor: "pink",
-//     borderRadius: "50%",
-//     scale: 0.5
-// })
+gsap.from('.box2', {
+    x: 500,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "pink",
+    borderRadius: "50%",
+    scale: 0.5,
+    yoyo: true,
+    repeat: -1
+})
 
 gsap.from('h1', {
     opacity: 0,
