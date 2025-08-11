df855d7a6ef28063fd848049328fd9fbfddfb471
Rajaryan singh <raj.aryan2518@gmail.com>
2025-08-11 15:45:08 +0530

    text animation

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-gsap.to('.box', {
-    x: 500,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: "pink",
-    borderRadius: "50%",
-    scale: 0.5
-})
+// gsap.to('.box', {
+//     x: 500,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     backgroundColor: "pink",
+//     borderRadius: "50%",
+//     scale: 0.5
+// })
 
-gsap.from('.box2', {
-    x: 500,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: "pink",
-    borderRadius: "50%",
-    scale: 0.5
-})
+// gsap.from('.box2', {
+//     x: 500,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     backgroundColor: "pink",
+//     borderRadius: "50%",
+//     scale: 0.5
+// })
 
+gsap.from('h1', {
+    opacity: 0,
+    duration: 1,
+    y: 30,
+    delay: 1,
+    stagger: -1
+
+})
 
